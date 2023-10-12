f5167cf0a8d63a8f20ae8c537c4a82e7f9e06156
Adam Žák <adam.zak@softpoint.sk>
2023-10-12 15:41:03 +0200

    Add version 2.0 (master)

--- kyiv_independent.py
+++ kyiv_independent.py
@@ -1,29 +1,37 @@
+import pandas as pd
 import requests
 import re
 from libraries import load_links
+from libraries import csv_manager
 
 
-def manage_links(rm_link, new_links, save_iterator, limit=10):
+def manage_links(rm_link, new_links, iterator, limit=10):
     processed_links_to_write.append(rm_link)
-    save_iterator += 1
+    iterator += 1
 
     for new_link in new_links:
-        if new_link not in processed_links:
+        if new_link not in processed_links and 'kyivindependent' in new_link:
             links.append(new_link)
             unprocessed_links_to_write.append(new_link)
 
-    if save_iterator >= limit:
+    if iterator >= limit:
+        print('Started storing data to files...')
+        csv_manager.store_data('data.csv', df)
+
         for writable_link in processed_links_to_write:
             processed_links_stack.write(writable_link + '\n')
 
         for writable_link in unprocessed_links_to_write:
             links_stack.write(writable_link + '\n')
 
-        save_iterator = 0
+        iterator = 0
         processed_links_to_write.clear()
         unprocessed_links_to_write.clear()
+        print('Ended file storing.')
 
     links.pop()
+
+    return iterator
 
 
 # Variable holding found links
@@ -34,6 +42,8 @@ unprocessed_links_to_write = []
 links_stack = open('independent_stack.txt', 'a+')
 processed_links_stack = open('independent_processed.txt', 'a+')
 save_iterator = 0
+df = csv_manager.load_data('data.csv')
+page_count = 0
 
 
 # URL of the web page you want to scrape
@@ -61,7 +71,7 @@ if response.status_code == 200:
         a_matches = re.search(a_pattern, h3_match)
         if a_matches is not None:
             links.append('https://kyivindependent.com' + str(a_matches.group(1)))
-            break
+            # break
 
     for href in links:
         print(href)
@@ -72,27 +82,39 @@ else:
 # Start scraping websites
 for link in links:
     response = requests.get(link)
+    print(link)
 
     if response.status_code != 200:
+        print(f'Error on link: {link}')
         continue
 
     html_content = response.text
 
     # Header
-    h1_pattern = r'<h1[^>]*>.*?</h1>'
+    h1_pattern = r'<(h1|h2)[^>]*>.*?</(h1|h2)>'
     h1_matches = re.search(h1_pattern, html_content, re.DOTALL)
     # Article
     article_pattern = r'<div class=\'c-content \'>.*<div id="reading-progress-end">'
     article_matches = re.search(article_pattern, html_content, re.DOTALL)
+
+    if article_matches.group(0) is None:
+        print(f'Link with no article: {link}')
+        continue
     # Links
     href_pattern = r'href=["\'](https?://[^"\']+)["\']'
     article_links = re.findall(href_pattern, article_matches.group(0), re.DOTALL)
 
-    print(article_matches.group(0))
+    #print(article_matches.group(0))
+    print(page_count)
     print(h1_matches.group(0))
-    print(article_links)
+    print('\n')
+    #print(article_links)
 
-    manage_links(link, article_links, save_iterator, 1)
+    df.loc[len(df)] = [str(h1_matches.group(0).replace('\t', ' ')), link, 'Ukraine', article_matches.group(0).replace('\t', ' ')]
+    save_iterator = manage_links(link, article_links, save_iterator, limit=50)
 
-    print(save_iterator)
-    exit(0)
+    page_count += 1
+    # exit(0)
+
+# Store the last values, after all links are processed
+manage_links('https://kyivindependent.com/tag/war/', [], 1000000000)
---
+++ libraries/csv_manager.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def load_data(filepath, sep='\t'):
+    df = pd.read_csv(filepath, sep=sep, index_col=None)
+    return df
+
+
+def store_data(filepath, df, sep='\t'):
+    try:
+        # Save the DataFrame to a CSV file
+        # df.to_csv(filepath, sep=sep, index=True, header=True)
+        df.to_csv(filepath, sep=sep)
+        print(f"Data has been successfully stored in {filepath}")
+    except Exception as e:
+        print(f"An error occurred while storing the data: {str(e)}")
