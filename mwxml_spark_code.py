b3abb7c7c42843ec3514bdc67680be52bc54ccb2
Adam Žák <adam.zak@softpoint.sk>
2023-11-23 15:53:47 +0100

    Add new spark code (master)

---
+++ mwxml_spark_code.py
@@ -0,0 +1,45 @@
+from pyspark.sql import SparkSession
+import mwxml
+import json
+import re
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py <path_to_single_dump>")
+    sys.exit(1)
+
+print('Riadok 22')
+# Create a Spark session
+spark = SparkSession.builder.appName("War Events").getOrCreate()
+
+# Load data name from input
+single_dump_path = sys.argv[1]
+
+# Broadcast the pattern variable to all nodes
+pattern = re.compile(r'Current events/.*')
+pattern_broadcast = spark.sparkContext.broadcast(pattern)
+
+# Read dump with mwxml library
+dump = mwxml.Dump.from_file(open(single_dump_path))
+
+# Crete dict which represents results JSON
+results = {}
+
+# Go over all pages in dump
+for page in dump:
+    # If title is Current events page
+    if pattern.match(page.title):
+        for revision in page:
+            # Add to the JSON, wher key is a date and value is text of page
+            results[page.title.split('/')[-1].replace(' ', '_')] = revision.text
+            print(page.title)
+
+# Save the results to a JSON file
+with open("events_json-azak.json", "w") as fp:
+    json.dump(results, fp)
+
+print('Script is successfully done :)')
+
+# Stop the Spark session
+spark.stop()
--- spark_code.py
+++ spark_code.py
@@ -1,45 +1,65 @@
-from pyspark.sql import SparkSession
-import mwxml
-import json
-import re
 import sys
+import re
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType
 
+# Start-up command:
+# spark-submit --packages com.databricks:spark-xml_2.12:0.17.0
+# --executor-memory 512m spark_code.py
+# ./spark/enwiki-latest-pages-articles1.xml-p1p41242
 
+# Check for the argument of data file
 if len(sys.argv) != 2:
     print("Usage: python script.py <path_to_single_dump>")
     sys.exit(1)
 
-print('Riadok 22')
-# Create a Spark session
-spark = SparkSession.builder.appName("War Events").getOrCreate()
+# Define REGEX to load only events data from page
+events_pattern = r'class=\"current-events-content description\">(.*?)<div class=\"current-events-nav\"'
+events_json = {}
 
-# Load data name from input
-single_dump_path = sys.argv[1]
+# Initialize Spark session
+spark = SparkSession.builder.appName("WikipediaSearch").getOrCreate()
 
-# Broadcast the pattern variable to all nodes
-pattern = re.compile(r'Current events/.*')
-pattern_broadcast = spark.sparkContext.broadcast(pattern)
+# Load Wikipedia dump data with databricks XML library
+wiki_df = spark.read.format("com.databricks.spark.xml") \
+    .option("rowTag", "page") \
+    .load(sys.argv[1])
 
-# Read dump with mwxml library
-dump = mwxml.Dump.from_file(open(single_dump_path))
+# Filter only Current Event pages
+current_event_df = wiki_df.filter(col("title").like("Portal:Current events/%"))
 
-# Crete dict which represents results JSON
-results = {}
+# Extract title and text
+title_text_df = current_event_df.select("title", "revision.text._VALUE")
 
-# Go over all pages in dump
-for page in dump:
-    # If title is Current events page
-    if pattern.match(page.title):
-        for revision in page:
-            # Add to the JSON, wher key is a date and value is text of page
-            results[page.title.split('/')[-1].replace(' ', '_')] = revision.text
-            print(page.title)
 
-# Save the results to a JSON file
-with open("events_json-azak.json", "w") as fp:
-    json.dump(results, fp)
+def format_date(title):
+    print(title)
+    return title.split('/')[-1].replace(' ', '_')
 
-print('Script is successfully done :)')
 
-# Stop the Spark session
-spark.stop()
+def format_text(text):
+    events_matches = re.search(events_pattern, text, re.DOTALL)
+    if events_matches is None:
+        return None
+    else:
+        return events_matches.group(1)
+
+
+# Create UDFs for the custom functions
+custom_title_udf = udf(format_date, StringType())
+custom_text_udf = udf(format_text, StringType())
+
+# Apply the UDFs to the title and text columns
+modified_title_text_df = title_text_df \
+    .withColumn("modified_title", custom_title_udf(col("title"))) \
+    .withColumn("modified_text", custom_text_udf(col("_VALUE"))) \
+    .select("modified_title", "modified_text")
+
+# Convert DF to RDD then to dict which will be stored as JSON
+output_dict = modified_title_text_df.rdd.collectAsMap()
+
+# Save the dictionary as a JSON file
+with open("events_json-azak.json", "w") as json_file:
+    json.dump(output_dict, json_file, ensure_ascii=False)
