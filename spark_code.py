a4da5aa8ba23927aac66970ca223ef9784ff2b54
Adam Žák <adam.zak@softpoint.sk>
2023-11-28 11:38:56 +0100

    Rework to spark code (master)

--- spark_code.py
+++ spark_code.py
@@ -1,9 +1,8 @@
 import sys
 import re
-import json
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, udf
-from pyspark.sql.types import StringType
+from pyspark.sql.types import StringType, StructType, StructField
 
 # Start-up command:
 # spark-submit --packages com.databricks:spark-xml_2.12:0.17.0
@@ -16,31 +15,41 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 # Define REGEX to load only events data from page
-events_pattern = r'class=\"current-events-content description\">(.*?)<div class=\"current-events-nav\"'
-events_json = {}
+events_pattern = r"<!-- All news items below this line -->.'(.*?)<!-- All news items above this line -->"
 
 # Initialize Spark session
 spark = SparkSession.builder.appName("WikipediaSearch").getOrCreate()
 
+# Define the custom schema for the DataFrame
+custom_schema = StructType([
+    StructField("title", StringType(), True),
+    StructField("revision", StructType([
+        StructField("text", StructType([
+            StructField("_VALUE", StringType(), True)
+        ]), True)
+    ]), True)
+])
+
 # Load Wikipedia dump data with databricks XML library
 wiki_df = spark.read.format("com.databricks.spark.xml") \
     .option("rowTag", "page") \
+    .schema(custom_schema) \
     .load(sys.argv[1])
 
 # Filter only Current Event pages
 current_event_df = wiki_df.filter(col("title").like("Portal:Current events/%"))
 
-# Extract title and text
-title_text_df = current_event_df.select("title", "revision.text._VALUE")
-
 
 def format_date(title):
-    print(title)
     return title.split('/')[-1].replace(' ', '_')
 
 
 def format_text(text):
+    if text is None:
+        return None
+
     events_matches = re.search(events_pattern, text, re.DOTALL)
+
     if events_matches is None:
         return None
     else:
@@ -52,14 +61,9 @@ custom_title_udf = udf(format_date, StringType())
 custom_text_udf = udf(format_text, StringType())
 
 # Apply the UDFs to the title and text columns
-modified_title_text_df = title_text_df \
+modified_title_text_df = current_event_df \
     .withColumn("modified_title", custom_title_udf(col("title"))) \
-    .withColumn("modified_text", custom_text_udf(col("_VALUE"))) \
+    .withColumn("modified_text", custom_text_udf(col("revision.text._VALUE"))) \
     .select("modified_title", "modified_text")
 
-# Convert DF to RDD then to dict which will be stored as JSON
-output_dict = modified_title_text_df.rdd.collectAsMap()
-
-# Save the dictionary as a JSON file
-with open("events_json-azak.json", "w") as json_file:
-    json.dump(output_dict, json_file, ensure_ascii=False)
+modified_title_text_df.write.json('events_json-azak', mode='overwrite')
